2ebecf7b4139893844de0ec3060043a12a989f5a
bailey drake & Kathryn Hough <student@epicodus.com>
2014-03-11 10:24:43 -0700

    add task class variable

--- lib/task.rb
+++ lib/task.rb
@@ -1,6 +1,27 @@
 require 'date'
 
 class Task
+
+  @@all_tasks = []
+
+  def Task.all
+    @@all_tasks
+  end
+
+  def Task.clear
+    @@all_tasks = []
+  end
+
+  def Task.create(description, priority=1, due_date)
+    new_task = Task.new(description, priority, due_date)
+    @@all_tasks << new_task
+    new_task
+  end
+
+  def save
+    @@all_tasks << self
+  end
+
   def initialize(description, priority=1, due_date)
     @description = description
     @priority = priority
--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -3,22 +3,40 @@ require 'task'
 require 'list'
 
 describe Task do
-  it 'is initialized with a description' do
-    test_task = Task.new('pet the llama')
-    test_task.should be_an_instance_of Task
+
+  before do
+    Task.clear
   end
-  it 'shows the description given' do
-    test_task = Task.new('pet the llama')
-    test_task.description.should eq 'pet the llama'
+  describe '.clear' do
+    it 'empties out all of that saved tasks' do
+      Task.new('wash the lion', '2014-03-10').save
+      Task.clear
+      Task.all.should eq []
+    end
   end
-  # it 'adds a priority number to the task' do
-  #   test_task = Task.new('annoy the lion')
-  #   test_task.priority.should eq 0
-  # end
-  # it 'adds a priority number to the task' do
-  #   test_task = Task.new('stroke the giraffe', 2)
-  #   test_task.priority.should eq 2
-  # end
+  describe '.all' do
+    it 'is empty at first' do
+      Task.all.should eq []
+   end
+ end
+ describe '#save' do
+  it 'adds a task to the array of saved tasks' do
+    test_task = Task.new('clean the llama', '2014-03-10')
+    test_task.save
+    Task.all.should eq [test_task]
+    end
+  end
+  describe '.create' do
+    it 'makes a new instance of Task' do
+      test_task = Task.create('wash the lion', '2014-03-10')
+      test_task.should be_an_instance_of Task
+    end
+
+    it 'saves the Task' do
+      test_task = Task.create('wash the lion', '2014-03-10')
+      Task.all.should eq [test_task]
+    end
+  end
 end
 
 describe List do
@@ -34,14 +52,14 @@ describe List do
 
   it 'should have an array of task objects' do
     test_list = List.new('chores')
-    test_task = Task.new('water the plants')
-    test_list.add_task(test_task)
+    test_task = Task.new('water the plants', '2014-03-10')
     test_list.task_list.length.should eq(1)
    end
 
-   it 'shuld sort task by priority' do
-    test_lit = List.new('chores')
-    test_task = Task.new('plant the tree', 2)
-    test_task = Task.new('water the hippo', 5)
-    test_list.sort.should eql(['water the hippo', 'plant the tree'])
+   it 'should sort task by priority' do
+    test_list = List.new('chores')
+    test_task = Task.new('plant the tree', 2, '2014-03-10')
+    test_task = Task.new('water the hippo', 5, '2014-03-10')
+    test_list.sort_by_priority.should eql(['water the hippo', 'plant the tree'])
+end
 end
